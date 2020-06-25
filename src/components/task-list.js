bf4582f53d0827368fd65dc9b110005882db8741
Carl Ola Fornander <ofornander@gmail.com>
2020-06-25 14:40:20 +0200

    Included buttons whose opacity increase during mouseover.

--- src/components/task-list.js
+++ src/components/task-list.js
@@ -20,9 +20,22 @@ function SetList(props) {
 }
 
 function SetTask(props) {
+
+    function increaseOpacity(event) {
+        event.target.style.opacity = 1;
+    }
+
+    function decreaseOpacity(event) {
+        event.target.style.opacity = 0.7;
+    }
+
     function handleChange(event) {
         event.preventDefault();
         props.update(props.task.id);
+    }
+
+    function takeTime(event) {
+        event.preventDefault();
     }
 
     if (props.task.isDone) {
@@ -34,17 +47,53 @@ function SetTask(props) {
             padding: "10px"}}
             onClick={handleChange}><del>
             {props.task.description}
-            </del></li>
+            </del>
+            <button style={{marginLeft: '1.0em',
+                border: "1.5px solid black",
+                borderRadius: "2px",
+                backgroundColor: "red",
+                color: "black",
+                padding: "5px 8px",
+                fontSize: '15px',
+                opacity: '0.7'}}
+                onMouseEnter={increaseOpacity}
+                onMouseLeave={decreaseOpacity}>
+                    Remove
+            </button>
+            </li>
         )}
     else {
         return (
-            <li key={props.task.id}
-            style={{color: 'black',
-            fontFamily: "Courier",
-            fontWeight: "bold",
-            padding: "10px"}}
-            onClick={handleChange}>
-            {props.task.description}
+            <li style={{color: 'black',
+                fontFamily: "Courier",
+                fontWeight: "bold",
+                padding: "10px"}}>
+            <span onClick={handleChange}
+                style={{marginRight: '1.0em'}}>
+                {props.task.description}</span>
+            <button style={{marginRight: '1.0em',
+                border: "1.5px solid black",
+                borderRadius: "2px",
+                backgroundColor: "yellow",
+                color: "black",
+                padding: "5px 8px",
+                fontSize: '15px',
+                opacity: '0.7'}}
+                onClick={takeTime}
+                onMouseEnter={increaseOpacity}
+                onMouseLeave={decreaseOpacity}>
+                Select
+            </button>
+            <button style={{border: "1.5px solid black",
+                borderRadius: "2px",
+                backgroundColor: "green",
+                color: "black",
+                padding: "5px 8px",
+                fontSize: '15px',
+                opacity: '0.7'}}
+                onClick={handleChange}
+                onMouseEnter={increaseOpacity}
+                onMouseLeave={decreaseOpacity}>Done</button>
             </li>
         )}
 }
